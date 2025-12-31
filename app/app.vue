2b2d8a3ec73aa58218cc78b224e35f2676294af8
Riva Almero <rivaalms@proton.me>
2025-12-31 13:00:59 +0700

    feat: add resume pdf

--- app/app.vue
+++ app/app.vue
@@ -19,9 +19,10 @@ onBeforeMount(() => {
    const cursor = document.getElementById("cursor")
    if (!cursor) return
    document.addEventListener("mousemove", (e) => {
+      if (document.body.clientWidth < 768) return
       let x = e.pageX
       let y = e.pageY
-      cursor.style.background = `radial-gradient(circle at ${x}px ${y}px, oklch(62.3% 0.214 259.815 / 10%), transparent 25%)`
+      cursor.style.background = `radial-gradient(circle at ${x}px ${y}px, oklch(69.6% 0.17 162.48 / 20%), transparent 50%)`
    })
 })
 </script>
@@ -31,24 +32,35 @@ onBeforeMount(() => {
       <NuxtRouteAnnouncer />
       <div
          id="cursor"
-         class="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute"
+         class="pointer-events-none fixed inset-0 -z-10 transition duration-300 lg:absolute hidden lg:block"
+         style="
+            background: radial-gradient(
+               circle at 0px 0px,
+               oklch(69.6% 0.17 162.48 / 20%),
+               transparent 50%
+            );
+         "
       />
-      <div class="mx-auto max-w-6xl min-h-screen">
+      <div class="mx-auto max-w-6xl min-h-screen px-6 py-12 lg:py-0">
          <div class="lg:flex lg:justify-between lg:gap-4">
             <header
                class="lg:sticky lg:top-0 lg:flex lg:flex-col lg:max-h-screen lg:w-[48%] lg:justify-between lg:py-24"
             >
                <div>
-                  <h1 class="text-5xl font-bold tracking-wide text-gray-50">
+                  <h1
+                     class="text-4xl lg:text-5xl font-bold tracking-wide text-slate-50"
+                  >
                      Riva Almero
                   </h1>
-                  <h2 class="font-medium text-xl mt-1">Web Developer</h2>
+                  <h2 class="font-medium text-lg lg:text-xl mt-1">
+                     Web Developer
+                  </h2>
                   <p class="max-w-sm mt-4">
                      Just like in photography, I frame, focus, and capture. Only
                      this time &ndash; with code.
                   </p>
                </div>
-               <ul class="flex items-center gap-2">
+               <ul class="flex items-center gap-2.5 mt-12 lg:mt-0">
                   <li class="shrink-0">
                      <a
                         class="block"
@@ -86,12 +98,21 @@ onBeforeMount(() => {
             </header>
             <main
                id="content"
-               class="lg:w-[52%] lg:py-24"
+               class="lg:w-[52%] py-24 pb-0 lg:pb-24"
             >
                <section
                   id="about"
                   class="mb-36"
                >
+                  <div
+                     class="sticky top-0 lg:hidden -mx-6 px-6 py-4 bg-slate-950/75 backdrop-blur"
+                  >
+                     <h2
+                        class="uppercase text-sm font-bold text-slate-200 tracking-widest"
+                     >
+                        About
+                     </h2>
+                  </div>
                   <p class="text-pretty mb-4 leading-relaxed">
                      My name is Riva Almero. I'm a front-end web developer based
                      in Bandung, Indonesia. I'm passionate about crafting
@@ -103,7 +124,8 @@ onBeforeMount(() => {
                      Programmer at
                      <a
                         href="https://jasamedikatransmedic.com/"
-                        class="text-gray-50 hover:text-emerald-400 font-medium"
+                        target="_blank"
+                        class="text-slate-50 hover:text-emerald-400 font-medium"
                         >Jasa Medika Transmedic</a
                      >, alongside managing freelance clients and side projects.
                      This balance allows me to continuously sharpen my skills,
@@ -128,6 +150,15 @@ onBeforeMount(() => {
                   id="experiences"
                   class="mb-36"
                >
+                  <div
+                     class="sticky top-0 lg:hidden -mx-6 px-6 py-4 bg-slate-950/75 backdrop-blur"
+                  >
+                     <h2
+                        class="uppercase text-sm font-bold text-slate-200 tracking-widest"
+                     >
+                        Experience
+                     </h2>
+                  </div>
                   <div>
                      <ol class="group/list">
                         <li
@@ -138,18 +169,18 @@ onBeforeMount(() => {
                               :href="exp.companyLink"
                               target="_blank"
                               rel="noreferrer noopener"
-                              class="grid grid-cols-8 gap-4 hover:opacity-100 group-hover/list:opacity-50 transition group/item"
+                              class="grid grid-cols-8 gap-4 gap-y-0 lg:gap-y-4 hover:opacity-100 group-hover/list:opacity-50 transition group/item"
                            >
-                              <div class="col-span-2">
+                              <div class="col-span-full md:col-span-2">
                                  <span
-                                    class="text-xs font-medium text-gray-500 uppercase tracking-wide"
+                                    class="text-xs font-medium text-slate-500 uppercase tracking-wide"
                                  >
                                     {{ exp.from }} &mdash; {{ exp.to }}
                                  </span>
                               </div>
-                              <div class="col-span-6">
+                              <div class="col-span-full md:col-span-6">
                                  <h3
-                                    class="font-medium leading-snug text-gray-200 group-hover/item:text-emerald-400 transition"
+                                    class="font-medium leading-snug text-slate-200 group-hover/item:text-emerald-400 transition"
                                  >
                                     {{ exp.company }}
                                     <Icon
@@ -157,7 +188,7 @@ onBeforeMount(() => {
                                        class="align-middle translate-y-px group-hover/item:-translate-y-1 group-hover/item:translate-x-1 transition-transform"
                                     />
                                  </h3>
-                                 <h4 class="text-gray-500 font-medium text-sm">
+                                 <h4 class="text-slate-500 font-medium text-sm">
                                     {{ exp.position }}
                                  </h4>
                                  <p
@@ -165,7 +196,7 @@ onBeforeMount(() => {
                                  >
                                     {{ exp.description }}
                                  </p>
-                                 <ul class="flex flex-wrap gap-2 mt-2">
+                                 <ul class="flex flex-wrap gap-2 mt-4">
                                     <li v-for="skill in exp.skills">
                                        <div
                                           class="flex items-center rounded-full bg-emerald-600/10 px-3 py-1 text-xs font-medium leading-5 text-emerald-400"
@@ -180,8 +211,10 @@ onBeforeMount(() => {
                      </ol>
                      <div class="mt-12">
                         <a
-                           href=""
-                           class="text-gray-200 hover:text-emerald-400 transition inline-flex items-baseline font-medium leading-tight group/link"
+                           href="/resume.pdf"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                           class="text-slate-200 hover:text-emerald-400 transition inline-flex items-baseline font-medium leading-tight group/link"
                         >
                            <span class="inline-block">
                               View full résumé
@@ -194,9 +227,10 @@ onBeforeMount(() => {
                      </div>
                   </div>
                </section>
-               <footer class="max-w-md pb-16 sm:pb-0 text-sm float-right">
+               <footer class="max-w-md text-sm">
                   <p>
-                     &copy; {{ new Date().getFullYear() }} Riva Almero. All rights reserved.
+                     &copy; {{ new Date().getFullYear() }} Riva Almero. All
+                     rights reserved.
                   </p>
                </footer>
             </main>
